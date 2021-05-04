7faae05a4e073e06a3a961fc4b3d8c5e7b9b14c7
Darshan D <darshand2000@gmail.com>
2021-05-04 07:58:07 +0530

    demo files

--- demo_client_4.cpp
+++ demo_client_4.cpp
@@ -4,12 +4,23 @@
 
 using namespace std;
 
+bool IsOdd (int i) 
+{
+  return ((i%2)==1);
+}
+
+bool IsEven (int i) 
+{
+  return ((i%2)==0);
+}
+
 int main()
 {
-    // Traversals
-    // Search
+    // Iterators
+    // Algorithms
+    
     cout<<"\n--------------------------------------------------------------------------------------------------------------------------------\n\n";
-
+    srand(16);
     Treap_t<int> t_1;
     t_1.insert(19);
     t_1.insert(23);
@@ -55,17 +66,86 @@ int main()
 
     cout<<"Tree T_1:\n\n"<<t_1<<"\n";
     cout<<"\n--------------------------------------------------------------------------------------------------------------------------------\n\n";
-    cout<<"Inorder Traversal of Tree T_1:\n\n";
-    t_1.inorder();
+
+    cout<<"Tree T_3:\n\n"<<t_3<<"\n";
     cout<<"\n--------------------------------------------------------------------------------------------------------------------------------\n\n";
-    cout<<"Preorder Traversal of Tree T_1:\n\n";
-    t_1.preorder();
-    cout<<"\n--------------------------------------------------------------------------------------------------------------------------------\n\n";
-    cout<<"Postorder Traversal of Tree T_1:\n\n";
-    t_1.postorder();
+    
+    cout<<"Generic Find\n\n";
+
+    Treap_t<int>::Iterator it_1 = find(t_1.begin(), t_1.end(), 23);
+    if(it_1 != t_1.end())
+    {
+        cout << "Key " << *it_1 << " found in T_1" << "\n\n";
+    }
+    else
+    {
+        cout << "Key " << 23 << " not found in T_1" << "\n\n";
+    }
+
+    Treap_t<int>::Iterator it_2 = find(t_1.begin(), t_1.end(), 364);
+
+    if(it_2 != t_1.end())
+    {
+        cout << "Found key " << *it_2 << " in T_1" << "\n\n";
+    }
+    else
+    {
+        cout << "Key " << 364 << " not found in T_1" << "\n\n";
+    }
+
     cout<<"\n--------------------------------------------------------------------------------------------------------------------------------\n\n";
 
+    cout<<"Member Find\n\n";
+    Treap_t<int>::Iterator it_3 = t_1.find(t_1.begin(), t_1.end(), 23);
 
+    if(it_3 != t_1.end())
+    {
+        cout << "Found key " << *it_3 << " in T_1" << "\n\n";
+    }
+    else
+    {
+        cout << "Key " << 23 << " not found in T_1" << "\n\n";
+    }
+
+    cout<<"\n--------------------------------------------------------------------------------------------------------------------------------\n\n";
+
+    cout<<"Generic Find_if\n\n";
+
+    Treap_t<int>::Iterator it_4 = find_if(t_1.begin(), t_1.end(), IsEven);
+
+    if(it_4 != t_1.end())
+    {
+        cout << "Found Even key " << *it_4 << " in T_1" << "\n\n";
+    }
+    else
+    {
+        cout << "Even Key not found in T_1" << "\n\n";
+    }
+
+
+    cout<<"\n--------------------------------------------------------------------------------------------------------------------------------\n\n";
+
+    cout<<"Member Replace\n\n";
+
+    t_3.replace(t_3.begin(), t_3.end(), "B", "H");
+    t_3.replace(t_3.begin(), t_3.end(), "D", "F");
+    t_3.replace(t_3.begin(), t_3.end(), "Y", "K");
+
+    cout<<"T_3 after replacing B with H and D with F and trying to replace a non existent key Y with K\n" << t_3 << "\n\n";
+
+    cout<<"\n--------------------------------------------------------------------------------------------------------------------------------\n\n";
+
+    cout<<"Generic Count and Count_if\n\n";
+
+    int c_1 = count(t_1.begin(), t_1.end(), 5);
+
+    cout<<"Count of key " << 5 << " in T_1: " << c_1 << "\n\n";
+
+    int c_2 = count_if(t_1.begin(), t_1.end(), IsOdd);
+
+    cout<<"Count of Odd keys" << " in T_1 : " << c_2 << "\n\n";
+
+    cout<<"\n--------------------------------------------------------------------------------------------------------------------------------\n\n";
 }
 
 
