b2acfbd9b6302bea7af27f9f5f26596d6d4603b8
mayurpeshve2@gmail.com <mayurpeshve2@gmail.com>
2021-05-04 08:02:56 +0530

    Fix split bug

--- demo_final_2.cpp
+++ demo_final_2.cpp
@@ -167,25 +167,12 @@ class Treap_node_t
     }
 
     //function to split treap
-    Treap_node_t* split_node(Treap_node_t **left_sub_treap_root, Treap_node_t **right_sub_treap_root)
+    void split_node(Treap_node_t **left_sub_treap_root, Treap_node_t **right_sub_treap_root)
     {
-        if(!this->duplicate)
-        {
-            *left_sub_treap_root = this->left_n;
-            *right_sub_treap_root = this->right_n;
-            this->left_n = nullptr;
-            this->right_n = nullptr;
-            return this;
-        }
-        else
-        {
-            *left_sub_treap_root = this->left_n;
-            *right_sub_treap_root = this;
-            // *right_sub_treap_root = (*right_sub_treap_root)->delete_node(*right_sub_treap_root, this->node_val.first);
-            // *right_sub_treap_root = (*right_sub_treap_root)->insert_node(*right_sub_treap_root, this->node_val.first, rand()%100+1);
-            this->left_n = nullptr;
-            return nullptr;
-        }
+        *left_sub_treap_root = this->left_n;
+        *right_sub_treap_root = this->right_n;
+        this->left_n = nullptr;
+        this->right_n = nullptr;
     }
 
     //function to merge two treaps
@@ -243,8 +230,9 @@ class Treap_node_t
 
         Treap_node_t *left_temp, *right_temp, *root;
 
-        right_treap = right_treap->insert_node(right_treap, left_treap->node_val.first, 110);
+        right_treap = right_treap->insert_node(right_treap, left_treap->node_val.first, 101+rand()%100);
         right_treap->split_node(&left_temp, &right_temp);
+        
         // cout<<"Before new \n";
         root = new Treap_node_t(*left_treap);
         // cout<<"After new \n";
@@ -749,14 +737,8 @@ class Treap_t
     //function to split a treap
     void split(key_t key, Treap_t *left_sub_treap, Treap_t *right_sub_treap)
     {
-        this->root = this->root->insert_node(this->root, key, 110);
-        this->root = this->root->split_node(&(left_sub_treap->root),&(right_sub_treap->root));
-        if(this->root == nullptr)
-        {
-            // right_sub_treap->delete_(key);
-            // cout<<"RST\n"<<*right_sub_treap;
-            right_sub_treap->insert(key);
-        }
+        this->root = this->root->insert_node(this->root, key, 101+rand()%100);
+        this->root->split_node(&(left_sub_treap->root),&(right_sub_treap->root));
         
     }
 
